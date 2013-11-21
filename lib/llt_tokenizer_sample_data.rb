38f7ef6ec7caed42b5dfc8177a4b6245a2735c4d
LFDM <1986gh@gmail.com>
2013-11-21 14:55:04 +0100

    Adds test environment.

--- lib/llt_tokenizer_sample_data.rb
+++ lib/llt_tokenizer_sample_data.rb
@@ -1,5 +1,29 @@
 require "llt_tokenizer_sample_data/version"
+require 'llt_tokenizer_sample_data/test_files'
+require 'llt'
+
+require 'pry'
+require 'forwardable'
 
 module LltTokenizerSampleData
-  # Your code goes here...
+  class Test
+    extend Forwardable
+
+    def_delegators :segmenter, :segment
+    def_delegators :tokenizer, :tokenize
+
+    def initialize
+      @files = TestFiles.new
+      @segmenter = services.fetch(:segmenter)
+      @tokenizer = services.fetch(:tokenizer)
+    end
+
+    def services
+      LLT::Service
+    end
+
+    def run
+      binding.pry
+    end
+  end
 end
